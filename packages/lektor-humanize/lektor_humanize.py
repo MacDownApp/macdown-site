8134b407a4ee1c5d748bf61e0820e650f3af5a4f
Tzu-ping Chung <uranusjr@gmail.com>
2017-01-08 06:33:33 +0800

    Add release page and some entries

---
+++ packages/lektor-humanize/lektor_humanize.py
@@ -0,0 +1,25 @@
+from lektor.pluginsystem import Plugin
+
+
+def volumize(value):
+    try:
+        value = float(value)
+    except ValueError:
+        value = 0
+    if value == 0:
+        return '0 bytes'
+    elif value == 1:
+        return '1 byte'
+    for unit in ('bytes', 'KB', 'MB', 'GB',):
+        if -1024 < value < 1024:
+            return '{value:3.1f} {unit}'.format(value=value, unit=unit)
+        value /= 1024
+    return '{value:3.1f} {unit}'.format(value=value, unit='TB')
+
+
+class HumanizePlugin(Plugin):
+
+    name = 'Humanize'
+
+    def on_setup_env(self, **extra):
+        self.env.jinja_env.filters['volumize'] = volumize
---
+++ packages/lektor-humanize/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lektor-humanize',
+    version='0.1',
+    py_modules=['lektor_humanize'],
+    entry_points={
+        'lektor.plugins': [
+            'humanize = lektor_humanize:HumanizePlugin',
+        ]
+    },
+)
