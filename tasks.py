61d8d2ece7c156e737cc7015dda15c423ac518ca
Tzu-ping Chung <uranusjr@gmail.com>
2017-01-16 23:10:03 +0800

    Add script to generate a new post template

--- tasks.py
+++ tasks.py
@@ -1,14 +1,20 @@
 from __future__ import unicode_literals
 
+import datetime
 import io
 import os
 
 import invoke
+import six
 
 from lektor.builder import Builder
 from lektor.cli import Context
 from lektor.db import F
+from lektor.project import Project
 from lektor.reporter import CliReporter
+
+if six.PY2:
+    input = raw_input   # noqa
 
 
 ROOT_DIR = os.path.dirname(__file__)
@@ -69,6 +75,7 @@ def build(ctx):
 
     env = lektor_cli_ctx.get_env()
     pad = env.new_pad()
+
     output_to = 'build'
 
     # This is essentially `lektor build --output-path build`.
@@ -100,3 +107,45 @@ def build(ctx):
             f.write('# Latests version download linkt.')
             f.write(latest_redirect)
             f.write('\n')
+
+
+blog_post_template = """
+id: {id}
+---
+title:
+---
+author: Tzu-ping Chung
+---
+pub_date: {today}
+---
+content:
+"""[1:]
+
+
+@invoke.task
+def post(ctx, slug=None):
+    project = Project.discover()
+    env = project.make_env()
+    pad = env.new_pad()
+
+    blog = pad.get('/blog')
+    latest_post = blog.children.first()
+    if latest_post is None:
+        next_id = 1
+    else:
+        next_id = latest_post['id'] + 1
+
+    if slug is None:
+        slug = input('Enter slug: ') or '__new_post__'
+
+    new_dirname = os.path.join(
+        ROOT_DIR, os.path.dirname(blog.source_filename), slug,
+    )
+    os.mkdir(new_dirname)
+
+    contents_fn = os.path.join(new_dirname, 'contents.lr')
+    with io.open(contents_fn, mode='w', encoding='utf8') as f:
+        f.write(blog_post_template.format(
+            id=next_id,
+            today=datetime.date.today().strftime(r'%Y-%m-%d'),
+        ))
